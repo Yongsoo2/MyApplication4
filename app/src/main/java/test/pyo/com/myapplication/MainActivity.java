15d18681f553ef018619953ebf223f4254f1b800
yongsoo <vydydtn2@naver.com>
2017-11-23 16:18:12 +0900

    test update

--- app/src/main/java/test/pyo/com/myapplication/MainActivity.java
+++ app/src/main/java/test/pyo/com/myapplication/MainActivity.java
@@ -2,12 +2,24 @@ package test.pyo.com.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.text_tv);
+
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("Hi Hi Hi");
+            }
+        });
     }
 }
