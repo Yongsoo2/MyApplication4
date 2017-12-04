42e430467e825c172fac9faa496fe57f7ed17ecd
yongsoo <vydydtn2@naver.com>
2017-12-04 17:11:45 +0900

    JitPack 배포 Test4 ( MainActivity 삭제 )

---
+++ app/src/main/java/test/pyo/com/myapplication/BitmapResize.java
@@ -0,0 +1,40 @@
+package test.pyo.com.myapplication;
+
+import android.graphics.Bitmap;
+import android.util.Log;
+
+/**
+ * Created by pyo on 2017-12-04.
+ */
+
+public class BitmapResize {
+
+    public Bitmap resizeBitmap(Bitmap bitmap, int maxResolution) {
+
+        int width = bitmap.getWidth();
+        int height = bitmap.getHeight();
+        int newWidth = 0, newHeight = 0;
+        float rate = 0.0f;
+
+        if (width > height) {
+            if (maxResolution < width) {
+                rate = maxResolution / (float) width;
+                newHeight = (int) (height * rate);
+                newWidth = maxResolution;
+            }
+        } else {
+            if (maxResolution < height) {
+                rate = maxResolution / (float) height;
+                newWidth = (int) (width * rate);
+                newHeight = maxResolution;
+            }
+        }
+
+        if (newWidth == 0 || newHeight == 0)
+            return bitmap;
+        Log.e("resize width :", String.valueOf(newWidth));
+        Log.e("resize height :", String.valueOf(newHeight));
+
+        return Bitmap.createScaledBitmap(bitmap, newWidth, newHeight, true);
+    }
+}
