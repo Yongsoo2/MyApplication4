3b44b0354da92a5f1559e0b815ea297adbef88fa
yongsoo <vydydtn2@naver.com>
2017-12-13 14:31:38 +0900

    JitPack 배포 Test5 (android 버전 업)

--- app/src/main/java/test/pyo/com/myapplication/Test.java
+++ app/src/main/java/test/pyo/com/myapplication/Test.java
@@ -1,6 +1,12 @@
 package test.pyo.com.myapplication;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by pyo on 2017-11-29.
@@ -11,5 +17,52 @@ public class Test {
         String test = "test method";
         Log.e("Test class", test);
     }
+
+    // EditText에 TextView 숨김 처리
+    public void textViewHide(final EditText editText, final TextView textView){
+        editText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                if ( i == keyEvent.KEYCODE_DEL){
+                    textView.setVisibility(View.GONE);
+                }
+                return false;
+            }
+        });
+
+        // editText 변화시
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String s = editable.toString();
+
+                if (s.equals("")){
+                    editText.setOnKeyListener(new View.OnKeyListener() {
+                        @Override
+                        public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                            if (i == keyEvent.KEYCODE_DEL) {
+                                textView.setVisibility(View.GONE);
+                            }
+                            return false;
+                        }
+                    });
+                }
+            }
+        });
+
+    }
+
+
+
 
 }
